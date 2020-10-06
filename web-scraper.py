e01d20daeca9d41a2fc3862c257d3785f0cd282e
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-06 20:03:23 +0300

    refactored code

--- web-scraper.py
+++ web-scraper.py
@@ -8,7 +8,6 @@ def get_jobs():
     URL = 'https://djinni.co/jobs/keyword-java/'
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup.findAll('div', class_='list-jobs__title'))
     return soup.findAll('div', class_='list-jobs__title')
 
 
@@ -18,32 +17,33 @@ def get_job_details():
     for job in jobs:
         title = job.find('a', class_='profile')
         href = job.find('a', href=True)
-        print('Job details gotten')
         job_details += title.text.strip() + ' https://djinni.co' + href['href'] + '\n'
-    print(job_details)
     return job_details
 
 
+# This method defines an SMTP client session object
+# which is used to send mail to a Gmail email.
+# Valid data must be set as the server.login()
+# and server.sendmail() parameters for the code to work.
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    # insert valid data for server login
     server.login('sample_email@gmail.com', 'sample Google app password')
     subject = 'Active Java Vacancies'
     body = get_job_details()
     msg = f"Subject: {subject} \n\n{body}"
     server.sendmail(
-        'sample_email@gmail.com',
-        'sample_email@gmail.com',
+        'from_sample_email@gmail.com',
+        'to_sample_email@gmail.com',
         msg
     )
     print('Email has been sent')
     server.quit()
 
 
+# Emails will be sent once a day (every 86,400 seconds).
 while True:
     send_mail()
-    # emails will be sent once a day
     time.sleep(86400)
